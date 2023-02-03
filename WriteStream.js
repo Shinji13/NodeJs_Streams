37a0b2dd54dfb8fbd969670e3a531815c6f7aaa0
Shinji13 <105009019+Shinji13@users.noreply.github.com>
2023-02-03 22:45:22 +0100

    I added the implementation for constructor function what i did is to push any method call before initialization to an event than emit the event with a callback that also changes the state of the stream.furthermore i updated the write and end method to check for close and initialized status and i dont think i will update again.

--- WriteStream.js
+++ WriteStream.js
@@ -2,24 +2,36 @@ const fs=require("fs")
 const {Event}=require("./event")
 class customStream extends Event{
       constructor({highWaterMark,methods:{_construct,_destroy,_write}}){
-          super({drain:[],finish:[],close:[],error:[]})
+          super({drain:[],finish:[],close:[],error:[],init:[]})
           this.highWaterMark=highWaterMark
-          this.writable=true
-          this.closed=false
+          this.initliazied=false
+          this.writable=false
+          this.closed=true
           this._destroy=_destroy
           this._write=_write
           this.internalBuffer=[]
           this.externalBuffer=[]
           this.byteRead=0
-          //this.Constructor(_construct)
+          this.Constructor(_construct)
       }
-      // Constructor(_construct){
-      //   if(typeof _construct!=="function"){
-      //       throw new Error("_construct must be implemented")
-      //   }   
-      //   _construct(this)
-      // }
+      Constructor(_construct){
+        if(typeof _construct!=="function"){
+            throw new Error("_construct must be implemented")
+        }   
+        _construct(()=>{
+           this.writable=true
+           this.closed=false
+           this.emit("init")
+        })
+      }
       write(chunk){
+         if(!this.initliazied){
+            this.AddEventListener("init",this.write.bind(this))   
+            return this.writable
+         }
+         if(this.closed){
+            throw new Error("the stream is closes")
+         }
          if(typeof this._write!=="function"){
                throw new Error("_write must be implemented")
          }
@@ -34,7 +46,7 @@ class customStream extends Event{
                   this.byteRead=0
                   this._write(data,this._onwrite.bind(this))
                }
-         } else if(!this.closed){
+         } else{
                this.externalBuffer.push(Buffer.from(chunk,"utf-8"))
          }
          return this.writable;
@@ -68,6 +80,12 @@ class customStream extends Event{
         this.byteRead=0
       }
       end(chunk){
+        if(!this.initliazied){
+            this.AddEventListener("init",this.end.bind(this))   
+         }
+         if(this.closed){
+            throw new Error("the stream is closes")
+         }
         if(typeof this._write!=="function"){
             throw new Error("_write must be implemented")
         }  
@@ -87,22 +105,7 @@ class customStream extends Event{
         if(typeof this._destroy!=="function"){
             throw new Error("_destroy must be implemented")
          }
+         this.cleanUp()
          this._destroy()
       }
 }
-
-let stream=new customStream({highWaterMark:8,methods:{_write:(chunk,onwrite)=>{
-     fs.writeFile("stream/test.txt",chunk,{flag:"a"},(err)=>{
-        onwrite(err)
-     })
-}}})
-stream.write("goooood byeee my friend ")
-stream.write("goooood byeee my friend ")
-stream.write("goooood byeee my friend ")
-stream.on("drain",()=>{
-  console.log("drained");
-})
-stream.end("see you later")
-stream.on("finish",()=>{
-  console.log("finished");
-})
